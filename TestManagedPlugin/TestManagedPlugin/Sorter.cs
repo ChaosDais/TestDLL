3c466e0058a642c89081338c28546208e30ff4a8
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2025-10-05 21:54:54 -0400

    .

---
+++ TestManagedPlugin/TestManagedPlugin/Sorter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+using System;
+
+namespace TestManagedPlugin
+{
+    public class Sorter : MonoBehaviour
+    {
+        public int[] a;
+
+        private void Start()
+        {
+            MeasureFunctionExecutionTime();
+        }
+
+        void MeasureFunctionExecutionTime()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            TestFunction();
+            stopwatch.Stop();
+
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+            UnityEngine.Debug.Log($"Managed plugin took {elapsedMilliseconds} ms to execute");
+        }
+
+        void TestFunction()
+        {
+            TestSort(a);
+        }
+
+        void TestSort(int[] a)
+        {
+            Array.Sort(a);
+        }
+    }
+}
